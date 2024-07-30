7a55be23663b6f5262decf32b6ac3dcf938a0ba1
Mohamed Yehia <orzamsok@gmail.com>
2024-07-30 23:46:38 +0300

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - The main function executes the puts function.
+ *
+ *
+ * Return: 0 if successful.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

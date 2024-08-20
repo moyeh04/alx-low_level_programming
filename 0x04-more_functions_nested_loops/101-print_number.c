614df374157d98e8e4057bfc5c7ea7061f62c559
Mohamed Yehia <orzamsok@gmail.com>
2024-08-20 03:11:35 +0300

    12. Numbers have life; they're not just symbols on paper

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,82 @@
+#include "main.h"
+
+/**
+ * count_digits - A function that counts the digits of @n.
+ *
+ * @n: Given number.
+ *
+ * Return: Number of digits
+ */
+
+int count_digits(int n)
+{
+	int digits = 0; /* Number of digits */
+
+	while (n > 0)
+	{
+		n /= 10;
+		digits++;
+	}
+
+	return (digits);
+}
+
+/**
+ * power_of_ten_from_digits - A function that generates a power
+ * of ten based on the number of digits.
+ *
+ * @n: Given number
+ *
+ * Return: @n as power of ten.
+ */
+
+int power_of_ten_from_digits(int n)
+{
+	int i;
+	int power_digits = 1;
+
+	for (i = 0; i < n; i++)
+		power_digits *= 10;
+
+	return (power_digits);
+}
+
+/**
+ * print_number - A function that prints an integer.
+ *
+ * @n: Given number.
+ *
+ * Return: Nothing(void...)
+ */
+
+void print_number(int n)
+{
+	int i;
+
+	if (n < 0) /* Handle negative numbers Explicitly */
+	{
+		n = abs(n);
+		_putchar('-');
+	}
+
+	int digits_count = count_digits(n);
+	int digits_as_p10 = power_of_ten_from_digits(digits_count);
+
+	/*printf("%d digits count:\n", digits_count);*/ /* Debugging */
+	/*printf("%d digits as p10:\n", digits_as_p10);*/ /* Debugging */
+
+
+	/**
+	 * We don't want to have 0 as a result
+	 * when printing.
+	 * Example: (456 -> 4.56 and not 0.456).
+	 * that's why we divide digits_as_p10 by 10.
+	 */
+
+	for (i = 0; i < digits_count; i++)
+	{
+		_putchar((n / (digits_as_p10 /= 10)) % 10 + '0');
+	}
+	_putchar('\n');
+
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,6 +6,7 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 int _isupper(int c);
@@ -18,5 +19,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
 
 #endif /*X04HEADERS*/

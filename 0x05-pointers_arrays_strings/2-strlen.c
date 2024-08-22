5615e7b78982e2081740508cde8246f75b9d44b9
Mohamed Yehia <orzamsok@gmail.com>
2024-08-23 02:20:43 +0300

    2. This report, by its very length, defends itself against the risk of being read
    Write a function that returns the length of a string.
    
    Prototype: int _strlen(char *s);
    FYI: The standard library provides a similar function: strlen. Run man
    strlen to learn more.
    
    julien@ubuntu:~/0x05$ cat 2-main.c
    #include "main.h"
    #include <stdio.h>
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        char *str;
        int len;
    
        str = "My first strlen!";
        len = _strlen(str);
        printf("%d\n", len);
        return (0);
    }
    julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89
    2-main.c 2-strlen.c -o 2-strlen
    julien@ubuntu:~/0x05$ ./2-strlen
    16
    julien@ubuntu:~/0x05$

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - A function that returns the length of a string.
+ *
+ * @s: The pointer pointing to string.
+ *
+ * Return: Length of a string.
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,10 +2,12 @@
 #define X05HEADERS
 
 #include <unistd.h>
+#include <string.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 
 #endif /*X05HEASERS*/
 

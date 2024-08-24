c46c0e21f9442949218eca93d3e67b53f66b3e87
Mohamed Yehia <orzamsok@gmail.com>
2024-08-24 21:29:32 +0300

    3. I do not fear computers. I fear the lack of them
    Write a function that prints a string, followed by a new line, to
    stdout.
    
    Prototype: void _puts(char *str);
    FYI: The standard library provides a similar function: puts. Run man
    puts to learn more.
    
    julien@ubuntu:~/0x05$ cat 3-main.c
    #include "main.h"
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        char *str;
    
        str = "I do not fear computers. I fear the lack of them - Isaac
    Asimov";
        _puts(str);
        return (0);
    }
    julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89
    _putchar.c 3-main.c 3-puts.c -o 3-puts
    julien@ubuntu:~/0x05$ ./3-puts
    I do not fear computers. I fear the lack of them - Isaac Asimov
    julien@ubuntu:~/0x05$

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - A functions that prints a string followed by a new line, to stdout
+ *
+ * @str: Pointer to the first character of the string.
+ *
+ * Return: Nothing(void...)
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}

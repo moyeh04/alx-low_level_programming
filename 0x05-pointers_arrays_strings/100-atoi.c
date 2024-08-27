b2fd7b3a41a563aaf413cf8a6e487c591e506fba
Mohamed Yehia <orzamsok@gmail.com>
2024-08-27 16:36:46 +0300

    10. Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers
    Write a function that convert a string to an integer.
    
    Prototype: int _atoi(char *s);
    The number in the string can be preceded by an infinite number of
    characters
    You need to take into account all the - and + signs before the number
    If there are no numbers in the string, the function must return 0
    You are not allowed to use long
    You are not allowed to declare new variables of “type” array
    You are not allowed to hard-code special values
    We will use the -fsanitize=signed-integer-overflow gcc flag to compile
    your code.
    FYI: The standard library provides a similar function: atoi. Run man
    atoi to learn more.
    
    julien@ubuntu:~/0x05$ cat 100-main.c
    #include "main.h"
    #include <stdio.h>
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        int nb;
    
        nb = _atoi("98");
        printf("%d\n", nb);
        nb = _atoi("-402");
        printf("%d\n", nb);
        nb = _atoi("          ------++++++-----+++++--98");
        printf("%d\n", nb);
        nb = _atoi("214748364");
        printf("%d\n", nb);
        nb = _atoi("0");
        printf("%d\n", nb);
        nb = _atoi("Suite 402");
        printf("%d\n", nb);
        nb = _atoi("         +      +    -    -98 Battery Street; San
    Francisco, CA 94111 - USA             ");
        printf("%d\n", nb);
        nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
        printf("%d\n", nb);
        return (0);
    }
    julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89
    -fsanitize=signed-integer-overflow 100-main.c 100-atoi.c -o 100-atoi
    julien@ubuntu:~/0x05$ ./100-atoi
    98
    -402
    -98
    214748364
    0
    402
    98
    402
    julien@ubuntu:~/0x05$

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _atoi - A function that converts a string to an integer.
+ *
+ * @s: Pointer to the first character of the string.
+ *
+ * Return: Integer from the string.
+ */
+
+int _atoi(char *s)
+{
+	int i;
+	int sign = +1;
+	int number;
+	int count = 0; /* Checks if the string has any integers */
+	int base = 10;
+
+	for (i = 0; s[i] != '\0'; s++)
+	{
+
+		if ((s[i] == ' ' || s[i] == '\t' || (s[i] < '0' || s[i] > '9')) && count > 0)
+			break;
+		/* To prevent any numbers that exist after the first contigous one */
+		else if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			count++;
+
+			if (count == 1)
+				number = 0;
+			/*
+			 * Initazlizing number to prevent contigous memory
+			 * if the function is called more than once
+			 */
+
+			number = number * base + (int)(s[i] - '0');
+		}
+	}
+	return (number * sign);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -16,6 +17,7 @@ void puts2(char *s);
 void puts_half(char *s);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif /*X05HEASERS*/
 

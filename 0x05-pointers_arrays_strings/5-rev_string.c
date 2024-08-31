0509ce1fac0581e5ec332ab111f09dab2e899954
Mohamed Yehia <orzamsok@gmail.com>
2024-09-01 00:45:53 +0300

    5. Wrong information.

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,10 +10,33 @@
 
 void rev_string(char *s)
 {
-	int len = strlen(s); /* This includes the null character */
+	int len = strlen(s); /* This doesn't include the null character */
 	int i = 0;
 	int j = len - 1;
 	char temp;
+
+	/**
+     * Let's break this down:
+     *
+     * 1. If our string is "hello":
+     *    - It has 5 characters
+     *    - But in memory, it's actually: ['h','e','l','l','o','\0']
+     *
+     * 2. strlen(s) gives us 5 (doesn't count '\0')
+     *
+     * 3. Array indices:
+     *    - Start from 0
+     *    - So valid indices are 0, 1, 2, 3, 4
+     *    - Last index is always (length - 1)
+     *
+     * 4. That's why we set j = len - 1:
+     *    - j = 5 - 1 = 4
+     *    - s[4] is the last character 'o'
+     *
+	 * 5. In a forward loop, we'd use:
+     *    - for (j = 0; j < len; j++)
+     *    - This goes from 0 to 4 for "hello"
+     */
 
 	while (i < j)
 	{

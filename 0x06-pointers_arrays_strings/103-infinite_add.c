68718e7ffa2627e038868fb8efdd3b922af6e202
Mohamed Yehia <orzamsok@gmail.com>
2024-09-01 19:33:41 +0300

    11. Separate Functions for manageability.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -25,6 +25,39 @@ void rev_string(char *rev)
 	}
 }
 
+/**
+ * handle_carry - A function that handles residual carry.
+ * @r : The buffer that the function will use to store the r.
+ * @big_num: Pointer to the rest of the number for padding.
+ * @i: The current position in the string.
+ * @carry: The current carry.
+ *
+ * Return: Nothing(void...)
+ */
+
+void handle_carry(char *r, char *big_num, int i, int carry)
+{
+	for (; big_num[i] != '\0'; i++) /* The padding loop */
+	{
+		if (carry == 1)
+			r[i] = (((big_num[i] - '0') + carry) % 10) + '0';
+		else if (carry == 0)
+			r[i] = (((big_num[i] - '0') + carry) % 10) + '0';
+
+		if (((big_num[i] - '0') + carry) > 9)
+			carry = 1;
+		else
+			carry = 0;
+	}
+	/* residual carry handle, and null terminator*/
+	if (carry == 0)
+		r[i] = '\0';
+	else if (carry == 1)
+	{
+		r[i] = '1';
+		r[i + 1] = '\0';
+	}
+}
 
 /**
  * infinite_add - A function that adds two numbers.
@@ -50,7 +83,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	strcpy(num2, n2);
 	rev_string(num1);
 	rev_string(num2);
-
 	if (strlen(num1) < strlen(num2))
 	{
 		sml_num = num1;
@@ -67,26 +99,15 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	for (i = 0; sml_num[i] != '\0'; i++)
 	{
-			r[i] = (((num1[i] - '0') + (num2[i] - '0') + carry) % 10) + '0';
+		r[i] = (((num1[i] - '0') + (num2[i] - '0') + carry) % 10) + '0';
 
-			if (((num1[i] - '0') + (num2[i] - '0') + carry) > 9)
-				carry = 1;
-			else
-				carry = 0;
+		if (((num1[i] - '0') + (num2[i] - '0') + carry) > 9)
+			carry = 1;
+		else
+			carry = 0;
 	}
-
-	/* residual carry handle, and null terminator*/
-	if (carry == 0)
-		r[i] = '\0';
-	else if (carry == 1)
-	{
-		r[i] = '1';
-		r[i + 1] = '\0';
-	}
-
-
+	handle_carry(r, big_num, i, carry);
 	rev_string(r);
-
 	if ((strlen(r) + 1) > (unsigned long int)size_r)
 		return (0);
 

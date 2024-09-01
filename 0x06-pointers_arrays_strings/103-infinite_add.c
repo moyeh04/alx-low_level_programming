64316bb78dac455a17abede6ace69f19187345d4
Mohamed Yehia <orzamsok@gmail.com>
2024-09-01 06:22:29 +0300

    11. Approach 1 -> No padding for smaller number.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -44,6 +44,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	char num1[strlen(n1)+1];
 	char num2[strlen(n2)+1];
 	char *sml_num;
+	char *big_num;
 
 	strcpy(num1, n1);
 	printf("%s\n", num1);
@@ -52,9 +53,15 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	rev_string(num2);
 
 	if (strlen(num1) < strlen(num2))
+	{
 		sml_num = num1;
+		big_num = num2;
+	}
 	else
+	{
 		sml_num = num2;
+		big_num = num1;
+	}
 
 	printf("%s\n", sml_num);
 	for (i = 0; sml_num[i] != '\0'; i++)
@@ -69,16 +76,23 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			else
 				carry = 0;
 	}
-	printf("r of [%d]:%d\n", i, strlen(r));
-	if (carry == 1)
-		r[i] = 1 + '0';
+	for (; big_num[i] != '\0'; i++)
+	{
+		if (carry == 1)
+		{
+			r[i] += ((big_num[i] - '0') + carry) + '0';
+		}
+		else if (carry == 0)
+		{
+			r[i] += ((big_num[i] - '0') + carry) + '0';
+		}
+		r[i] = big_num[i];
+	}
+	
 
 	rev_string(num1);
 	rev_string(num2);
 	rev_string(r);
-			printf("r of [%d]:%c\n", 11, r[10]);
-	printf("r of [%d]:%s\n", i, r);
-	printf("r of [%d]:%d\n", i, strlen(r));
 
 	if ((strlen(r) + 1) > size_r)
 		return (0);
@@ -95,7 +109,7 @@ int main(void)
 	char *res;
 
 	n = "999999989";
-	m = "111111111";
+	m = "1";
 	res = infinite_add(n, m, r2, 15);
 	if (res == 0)
 	{

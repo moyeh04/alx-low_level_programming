a814ea637a507f2f3c0d4736997908c05aa7db56
Mohamed Yehia <orzamsok@gmail.com>
2024-10-12 12:50:38 +0300

    Add 0x07 main.h Header file.

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef X07HEADERS
+#define X07HEADERS
+
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+#endif /* X07HEADERS */

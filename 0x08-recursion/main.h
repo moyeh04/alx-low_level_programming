a9ca615a094063b0f13d06d09f25adbe3431e99d
Mohamed Yehia <orzamsok@gmail.com>
2024-10-22 23:33:18 +0300

    Add 0x08 main.h Header file.

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef X08HEADERS
+#define X08HEADERS
+
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void _puts_recursion(char *s);
+
+#endif /* X08HEADERS */

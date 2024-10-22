665ee22a6c06767114095ba342950f7a61ed18a6
Mohamed Yehia <orzamsok@gmail.com>
2024-10-22 13:52:01 +0300

    6. Chess is mental torture
    Write a function that prints the chessboard.
    
    Prototype: void print_chessboard(char (*a)[8]);
    julien@ubuntu:~/0x07$ cat 7-main.c
    #include "main.h"
    #include <stdio.h>
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        char board[8][8] = {
            {'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
            {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
            {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
            {'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
        };
        print_chessboard(board);
        return (0);
    }
    julien@ubuntu:~/0x07$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89
    _putchar.c 7-main.c 7-print_chessboard.c -o 7-print_chessboard
    julien@ubuntu:~/0x07$ ./7-print_chessboard
    rkbqkbkr
    pppppppp
    
    PPPPPPPP
    RKBQKBKR
    julien@ubuntu:~/0x07$

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,5 +13,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int _putchar(char c);
+void print_chessboard(char (*a)[8]);
 
 #endif /* X07HEADERS */

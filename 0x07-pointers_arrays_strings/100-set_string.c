d6527ac04c19c11d44b7e1dcdbf2c293e9ac25cd
Mohamed Yehia <orzamsok@gmail.com>
2024-10-22 17:21:50 +0300

    9. Add Description

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -7,6 +7,12 @@
  * new string.
  * @to: A pointer to the original string.
  *
+ * Description: The trick in this code is that for the variable s to reference
+ * the address of another pointer, it must be a double pointer. This means s
+ * should be of type type**, where type is the data type of the pointer it
+ * points to. This allows s to hold the address of a pointer, enabling more
+ * complex data structures and memory management.
+ *
  * Return: Nothing(void...).
  */
 
